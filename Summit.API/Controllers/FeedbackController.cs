b174648c2e46ddc1bacd45486b90e6f36b49617b
karanbhanushali <karanbhanushali990@gmail.com>
2018-03-12 02:26:50 +0530

    Feedback and Image controller

---
+++ Summit.API/Controllers/FeedbackController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Summit.API.Models;
+
+namespace Summit.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class FeedbackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+      
+        [HttpPost]
+        public void Post(feedbackModel model)
+        {
+
+            string country = "Austin";
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(new
+            {
+                user_name = model.Email,
+                review = model.feedbackk,
+            });
+            var request = WebRequest.CreateHttp("https://annualsumm.firebaseio.com/Feedback/" + country + ".json");
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            var buffer = Encoding.UTF8.GetBytes(json);
+            request.ContentLength = buffer.Length;
+            request.GetRequestStream().Write(buffer, 0, buffer.Length);
+            var response = request.GetResponse();
+            json = (new StreamReader(response.GetResponseStream())).ReadToEnd();
+
+            
+
+        }
+
+
+
+    }
+}
