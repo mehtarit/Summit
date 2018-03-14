7d52b9cd2d2d3c59c7f15bdf24775bf254a06763
Ritika Mehta <mehtarit@umich.edu>
2018-03-14 09:27:49 -0500

    Resolved conflicts from ImageUploadController

--- Summit.API/Controllers/ImageUploadController.cs
+++ Summit.API/Controllers/ImageUploadController.cs
@@ -3,23 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-<<<<<<< HEAD
 using System.Web;
 using System.IO;
 using Firebase.Storage;
-=======
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web;
-using System.IO;
-using Firebase.Storage;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
->>>>>>> 280c94e15f959944f039ecac11f59f8e0f652142
 using System.IO;
 using Firebase.Storage;
 using System.Net;
@@ -33,17 +19,7 @@ namespace Summit.API.Controllers
     [Route("api/[controller]")]
     public class ImageUploadController : Controller
     {
-<<<<<<< HEAD
-       /* public IActionResult Index()
-        {
-            return View();
-        }*/
-=======
-        public IActionResult Index()
-        {
-            return View();
-        }
->>>>>>> 280c94e15f959944f039ecac11f59f8e0f652142
+
 
 
 
@@ -55,7 +31,7 @@ namespace Summit.API.Controllers
               .Select(s => s[random.Next(s.Length)]).ToArray());
         }
 
-        
+        //POST api/Images
         [HttpPost]
         public void Post(imageUploadModel model)
         {
