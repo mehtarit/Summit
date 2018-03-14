9cf8f89cb91644aeffa4a66f8bba5285ee50ec72
Ritika Mehta <mehtarit@umich.edu>
2018-03-14 03:17:04 -0500

    check these

---
+++ Summit.API/Models/imageUploadModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Summit.API.Models
+{
+    public class imageUploadModel
+    {
+
+        [Required]
+
+        public string filepath { get; set; }
+
+        public string targetURL { get; set; }
+        public string downloadURL { get; set; }
+    }
+}
